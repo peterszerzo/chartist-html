c1cc0845cb1b36923b5124356611d5327105ab2e
jessicamcinchak <jessica.mcinchak@gmail.com>
2015-05-07 16:35:31 -0400

    add series case for pie charts

--- build/chartist-html.js
+++ build/chartist-html.js
@@ -5,28 +5,80 @@ ChartistHtml.config = {
 	modifierClassFragment: '--',
 	seriesSeperator: ['|', ',']
 };
+ChartistHtml.getBaseClass = function() {
+	return this.config.baseClass;
+};
+
+ChartistHtml.getLabelsClass = function() {
+	return this.config.baseClass + this.config.elementClassFragment + 'labels';
+};
+
+ChartistHtml.getSeriesClass = function() {
+	return this.config.baseClass + this.config.elementClassFragment + 'series';
+};
+
+/*
+ * Splits string into array based on configured separator characters.
+ * Try all of them until one is found in the string.
+ * @param {string} string - String to be split.
+ * @returns {array} array - Split array. 
+ */
+ChartistHtml.splitString = function(string) {
+	var $anyString = $(this),
+		splitArray = [],
+		i, max;
+
+	for(i=0, max = anyString.length; i < max; i += 1) {
+		if (i === this.config.stringSeperator) {
+			$anyString.split(this.config.stringSeperator);
+		}
+	}
+	return splitArray;
+};
+	// loop over separator characters
+	// when you find one that is present in the string
+	//   interrupt the loop
+	//   do splitting by that character
+	// return
+
 ChartistHtml.htmlToJson = function(html) {
 
 	var $el = $(html),
-		$labelsEl = $($el.find('.ct-html__labels')),
-		$seriesEl = $($el.find('.ct-html__series')),
+		$labelsEl = $($el.find('.' + this.getLabelsClass())),
+		$seriesEl = $($el.find('.' + this.getSeriesClass())),
 		json = {};
 
 	json.title = $el.attr('data-title'); 
 	json.type = $el.attr('data-type');
-	json.options = $el.attr('data-options').split('|');
-
-	json.labels = $labelsEl.html().split('|');
+	
+	if (json.type !== 'pie') {
+		json.options = $el.attr('data-options').split('|');
+		json.labels = $labelsEl.html().split('|');
+	} else {
+		json.labels = [];
+	}
 
 	json.series = [];
 
 	$seriesEl.each(function() {
-		var stringSeries = $(this).html().split('|'),
-			numberSeries = [];
 
-		numberSeries = parseFloat(stringSeries);
-		json.series.push(numberSeries);
+		var $seriEl = $(this),
+			stringSeries = $seriEl.html().split('|'),
+			numberSeries = [],
+			i, max;
 
+		for(i = 0, max = stringSeries.length; i < max; i += 1) {
+			numberSeries.push(parseFloat(stringSeries[i]));
+		}
+
+		if ([ 'bar', 'line' ].indexOf(json.type) > -1) {
+			json.series.push(numberSeries);
+		} else if (json.type === 'pie') {
+			json.series.push(numberSeries[0]);
+			json.labels.push($seriEl.attr('data-name'));
+		} else {
+
+		}
 	});
 
 	return json;
--- spec/html-to-json-spec.js
+++ spec/html-to-json-spec.js
@@ -1,26 +1,46 @@
 describe('ChartistHtml', function() {
 
-	var html = '<div class="ct-html" data-title="A Fine Chart" data-type="bar" data-options="stacked|horizontal"><ul><li class="ct-html__labels">May|June|July|August|September</li><li class="ct-html__series" data-name="Federal">1|2|3|4|5</li><li class="ct-html__series" data-name="State">1|2|3|4|5</li><li class="ct-html__series" data-name="Local">1|2|3|4|5</li></ul></div>';
-	
 	describe('htmlToJson', function() {
-		it('detects chart title', function() {
-			(ChartistHtml.htmlToJson(html).title).should.equal('A Fine Chart');
+		describe('for bar charts', function() {
+			var html = '<div class="ct-html" data-title="A Fine Chart" data-type="bar" data-options="stacked|horizontal"><ul><li class="ct-html__labels">May|June|July|August|September</li><li class="ct-html__series" data-name="Federal">1|2|3|4|5</li><li class="ct-html__series" data-name="State">1|2|3|4|5</li><li class="ct-html__series" data-name="Local">1|2|3|4|5</li></ul></div>';
+			beforeEach(function() {
+				ChartistHtml.config.baseClass = 'ct-html';
+			});
+			it('detects chart title', function() {
+				(ChartistHtml.htmlToJson(html).title).should.equal('A Fine Chart');
+			});
+			it('detects chart type', function() {
+				(ChartistHtml.htmlToJson(html).type).should.equal('bar');
+			});
+			it('detects and separates chart options', function() {
+				(ChartistHtml.htmlToJson(html).options[0]).should.equal('stacked');
+				(ChartistHtml.htmlToJson(html).options[1]).should.equal('horizontal');
+			});
+			it('detects and separates chart labels', function() {
+				(ChartistHtml.htmlToJson(html).labels[0]).should.equal('May');
+			});
+			it('detects and separates chart series - array of array', function() {
+				(ChartistHtml.htmlToJson(html).series[0][0]).should.equal(1);
+			});
 		});
-		it('detects chart type', function() {
-			(ChartistHtml.htmlToJson(html).type).should.equal('bar');
+		
+		describe('for pie charts', function() {
+			var html = '<div class="cts" data-type="pie"><ul><li class="cts__series" data-name="Federal">25</li><li class="cts__series" data-name="State">50</li><li class="cts__series" data-name="Local">25</li></ul></div>';
+			beforeEach(function() {
+				ChartistHtml.config.baseClass = 'cts';
+			});
+			it('detects chart type', function() {
+				(ChartistHtml.htmlToJson(html).type).should.equal('pie');
+			});
+			it('detects and separates chart labels', function() {
+				(ChartistHtml.htmlToJson(html).labels[0]).should.equal('Federal');
+			});
+			it('detects and separates chart series - simple array', function() {
+				(ChartistHtml.htmlToJson(html).series[0]).should.equal(25);
+			});
 		});
-		it('detects and separates chart options', function() {
-			(ChartistHtml.htmlToJson(html).options[0]).should.equal('stacked');
-			(ChartistHtml.htmlToJson(html).options[1]).should.equal('horizontal');
-		});
-		it('detects and separates chart labels', function() {
-			(ChartistHtml.htmlToJson(html).labels[0]).should.equal('May');
-		});
-		it('detects and separates chart series', function() {
-			(ChartistHtml.htmlToJson(html).series[0]).should.equal(1);
-		});
-		describe('integration', function() {
-			
+
+		describe('integration', function() {	
 		});
 	});
 });
